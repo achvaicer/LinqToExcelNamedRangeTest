6b7280d05bb45de45f822b47cab9083d03d2e334
achvaicer <alberto.chvaicer@leste.com>
2015-08-21 09:23:44 -0300

    first commit

---
+++ LinqToExcelNamedRangeTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToExcelNamedRangeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"NamedRange.xlsb";
+            var excel = new ExcelQueryFactory(file);
+
+            var unique = excel.NamedRange("NamedRangeUniqueCell");
+            var multiple = excel.NamedRange("NamedRangeMultipleCells");
+
+        }
+    }
+}
